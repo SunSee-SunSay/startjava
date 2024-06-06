62e38d334679f369406bf30defdab14ef3eea8a9
Евгения Дмитриенко <evgeniadmitrienko@MacBook-Pro-Evgenia.local>
2024-06-06 15:17:26 +0300

    Итоговое ДЗ Калькулятор

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите значение первого числа: ");
+        int number1 = scanner.nextInt();
+
+        System.out.print("Введите знак: ");
+        char sign = scanner.next().charAt(0);
+
+        System.out.print("Введите значение второго числа: ");
+        int number2 = scanner.nextInt();
+
+        scanner.close();
+
+        int result = 1;
+        if (sign == '+') {
+            result = number1 + number2;
+        } else if (sign == '-') {
+            result = number1 - number2;
+        } else if (sign == '*') {
+            result = number1 * number2;
+        } else if (sign == '/') {
+            result = number1 / number2;
+        } else if (sign == '%') {
+            result = number1 % number2;
+        } else if (sign == '^') {
+            for (int i = 1; i <= number2; i++) {
+                result *= number1;
+            }
+        } else {
+            System.out.println("Введите корректный символ операции из доступных: +, -, *, /, ^, %");
+        }
+
+        System.out.println(number1 + " " + sign + " " + number2 + " = " + result);
+    }
+}
