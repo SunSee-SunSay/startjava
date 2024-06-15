e379f6d072fac6454c04bc82a337598b4ec4f26a
Евгения Дмитриенко <evgeniadmitrienko@MBP-Evgenia.Dlink>
2024-06-15 19:19:16 +0300

    ДЗ 2.2 Что такое класс

---
+++ Lesson_2/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+    String name = "Федя";
+    String sex = "муж";
+    int age = 12;
+    int height = 160;
+    float weight = 44.5f;
+
+    public void go() {
+        System.out.println("Идти");
+    }
+
+    public void sit() {
+        System.out.println("Сидеть");
+    }
+
+    public void run() {
+        System.out.println("Бежать");
+    }
+
+    public void speak() {
+        System.out.println("Говорить");
+    }
+
+    public void learnJava() {
+        System.out.println("Учить Java");
+    }
+}
